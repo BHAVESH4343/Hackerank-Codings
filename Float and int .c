68ccd14905b6f3b56eb51fd11f899e0e3eed609a
BHAVESH4343 <63332504+BHAVESH4343@users.noreply.github.com>
2020-05-27 08:40:20 +0530

    Update Float and int .c

--- Float and int .c
+++ Float and int .c
@@ -11,8 +11,8 @@ int main()
     scanf("%f %f",&m1,&m2);
     int s1=n1+n2;
     int s2=n1-n2;
-    int t1=m1+m2;
-    int t2=m1-m2;
+    float t1=m1+m2;
+    float t2=m1-m2;
     printf("\n%d %d\n%.1f %.1f",s1,s2,t1,t2);
     
     return 0;
